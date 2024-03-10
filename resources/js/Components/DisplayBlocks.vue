bc69fdb8b815d80495e9c1f402b7d6f33fecb8c4
Squeenz <91484577+Squeenz@users.noreply.github.com>
2024-03-10 22:09:34 +0000

    Reworking to make it more dynamic and re-useable

---
+++ resources/js/Components/DisplayBlocks.vue
@@ -0,0 +1,57 @@
+<script setup>
+
+const props = defineProps({
+    type: String,
+    content: Object,
+});
+
+</script>
+
+<template>
+    <div v-if="props.type === 'textBox'">
+        <h1 class="text-2xl italic">{{ props.content.title }}</h1>
+        <p>
+            {{ props.content.desc }}
+        </p>
+    </div>
+
+    <div v-if="props.type === 'gridList'">
+        <div class="flex flex-wrap justify-center m-2 gap-3">
+            <div v-for="item in props.content"
+                :key="item"
+                class="bg-[#1A1A1A] p-2 rounded-sm grid grid-flow-col w-[30rem] text-center justify-start"
+            >
+                <h1 class="bg-red-900 text-center w-[4rem] text-3xl rounded-sm py-[1rem]">{{ item.id }}</h1>
+                <p class="mx-[1rem] my-auto">
+                    {{ item.desc }}
+                </p>
+            </div>
+
+            <!-- <div class="bg-red-900 p-2 rounded-md grid grid-flow-col w-[30rem] justify-center">
+                <h1 class="my-auto text-5xl font-bold">AND MORE</h1>
+            </div> -->
+        </div>
+    </div>
+
+    <div v-if="props.type === 'specialList'">
+        <div class="flex flex-wrap justify-center text-white">
+            <div class="justify-center m-2 gap-3">
+                <div class="bg-red-800 relative rounded-full right-[50%] top-[5.6rem] h-[23rem] w-3 float-right hidden lg:inline-block"></div>
+
+                <div v-for="item in props.content" :key="item.id" class="bg-[#1A1A1A] p-2 rounded-sm grid grid-flow-col w-[30rem] mx-5 justify-start"
+                    :class="item.id % 2 ? 'float-left my-[3rem]' : 'float-right my-[5rem]'">
+
+                    <div class="my-auto h-5 w-0 relative" :class="item.id % 2 ? 'left-[29.6rem]' : 'right-[6.6rem]'">
+                        <div class="bg-red-800 rounded-full h-2 w-[6rem] hidden lg:inline-block"> </div>
+                    </div>
+
+                    <h1 class="bg-red-900 text-center w-[4rem] text-3xl rounded-sm py-[1rem]">{{ item.id }}</h1>
+
+                    <p class="mx-[1rem] my-auto">
+                        {{ item.desc }}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
+import DisplayBlocks from '@/Components/DisplayBlocks.vue';
 
 defineProps({
     canLogin: {
@@ -10,12 +11,37 @@ defineProps({
         type: Boolean,
     }
 });
+
+
+const text = {
+    title: "Why should you choose us?",
+    desc: "At Gamer Love, we believe in the power of shared interests, and what better foundation for a relationship than a love for gaming? Here's why you should choose us:",
+}
+
+
+const text2 = {
+    title: "How does it work?",
+    desc: "Simple, here's a breakdown",
+}
+
+const features = {
+    featuresList: [],
+}
+
+const createFeature = () => {
+    for (let n = 1; n <= 5; n++) {
+        features.featuresList.push({
+            id: n,
+            desc: `Description for Feature ${n}`
+        });
+    }
+}
+
+createFeature();
 </script>
 
 <template>
     <Head title="Welcome" />
-
-
     <GuestLayout :canLogin="canLogin" :canRegister="canRegister">
 
     <template #header>
@@ -25,54 +51,14 @@ defineProps({
     </template>
 
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="sm:py-[6.8rem] bg-[#0A0A0A] shadow rounded-sm">
+          <div class="sm:py-[1.8rem] bg-[#0A0A0A] shadow rounded-sm">
                 <div class="flex flex-wrap justify-center text-white">
-                    <h1 class="text-2xl italic">Why should you choose us?</h1>
-                    <p>
-                        At Gamer Love, we believe in the power of shared interests,
-                        and what better foundation for a relationship than a love for gaming?
-                         <br />Here's why you should choose us:
-                    </p>
-                    <div class="flex flex-wrap justify-center m-2 gap-3">
-                        <div v-for="n in 5"
-                            :key="n"
-                            class="bg-[#1A1A1A] p-2 rounded-sm grid grid-flow-col w-[30rem]"
-                        >
-                            <h1 class="bg-red-900 text-center w-[4rem] text-3xl rounded-sm pt-[1rem] mr-2">{{ n }}</h1>
-                            <p class="mx-auto my-auto">
-                                Gamer-Centric Community: We're a community built by and for gamers.
-                                We understand your world, your interests, and your passion for gaming.
-                            </p>
-                        </div>
+                    <DisplayBlocks type="textBox" :content="text"/>
+                    <DisplayBlocks type="gridList" :content="features.featuresList"/>
 
-                        <div class="bg-red-900 p-2 rounded-md grid grid-flow-col w-[30rem] justify-center">
-                            <h1 class="my-auto text-5xl font-bold">AND MORE</h1>
-                        </div>
-                    </div>
+                    <DisplayBlocks type="textBox" :content="text2"/>
+                    <DisplayBlocks type="specialList" :content="features.featuresList"/>
                 </div>
-
-                <div class="flex flex-wrap justify-center text-white">
-                    <h1 class="text-2xl italic">How does it work?</h1>
-                    <div class="justify-center m-2 gap-3">
-                        <div class="bg-red-800 relative rounded-full right-[50%] top-[6.3rem] h-[23.4rem] w-3 float-right hidden lg:inline-block"></div>
-
-                        <div v-for="n in 5" :key="n" class="bg-[#1A1A1A] p-2 rounded-sm grid grid-flow-col w-[30rem] mx-5"
-                            :class="n % 2 ? 'float-left my-[3rem]' : 'float-right my-[5rem]'">
-
-                            <div class="my-auto mr-2 h-1 w-4 relative" :class="n % 2 ? 'left-[29.6rem]' : 'right-[6.6rem]'">
-                                <div class="bg-red-800 rounded-full h-2 w-[6rem] hidden lg:inline-block"> </div>
-                            </div>
-
-                            <h1 class="bg-red-900 text-center w-[4rem] text-3xl rounded-sm pt-[1rem] mr-2">{{ n }}</h1>
-
-                            <p class="mx-auto my-auto">
-                                Gamer-Centric Community: We're a community built by and for gamers. We understand your world, your interests,
-                                and your passion for gaming.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
           </div>
     </div>
 
